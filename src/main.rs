4953905f8817c55c1c57bfd8056d6746ad64ebef
Phodal Huang <h@phodal.com>
2020-06-23 20:18:28 +0800

    refactor: remove unsued vars

--- src/main.rs
+++ src/main.rs
@@ -18,64 +18,36 @@ use rustbox::Key;
 /// This doc string acts as a help message when the user runs '--help'
 /// as do all doc strings on fields
 #[derive(Clap)]
-#[clap(version = "1.0", author = "Kevin K. <kbknapp@gmail.com>")]
 struct Opts {
-    /// Sets a custom config file. Could have been an Option<T> with no default too
-    #[clap(short, long, default_value = "default.conf")]
-    config: String,
-    /// Some input. Because this isn't an Option<T> it's required to be used
     input: String,
-    /// A level of verbosity, and can be used multiple times
-    #[clap(short, long, parse(from_occurrences))]
-    verbose: i32,
-
     #[clap(subcommand)]
     subcmd: SubCommand,
 }
 
 #[derive(Clap)]
 enum SubCommand {
-    #[clap(version = "1.3", author = "Someone E. <someone_else@other.com>")]
-    Test(Test),
-    UI(Test),
+    #[clap(version = "0.0.1")]
+    UI(EditorCmd),
+    Box(EditorCmd),
 }
 
 /// A subcommand for controlling testing
 #[derive(Clap)]
-struct Test {
-    /// Print debug info
-    #[clap(short)]
-    debug: bool
+struct EditorCmd {
+    /// Some input. Because this isn't an Option<T> it's required to be used
+    #[clap(short, long)]
+    path: String,
 }
 
 fn main() {
     let opts: Opts = Opts::parse();
-
-    // Gets a value for config if supplied by user, or defaults to "default.conf"
-    println!("Value for config: {}", opts.config);
-    println!("Using input file: {}", opts.input);
-
-    // Vary the output based on how many times the user used the "verbose" flag
-    // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
-    match opts.verbose {
-        0 => println!("No verbose info"),
-        1 => println!("Some verbose info"),
-        2 => println!("Tons of verbose info"),
-        3 | _ => println!("Don't be crazy"),
-    }
-
-    // You can handle information about subcommands by requesting their matches by name
-    // (as below), requesting just the name used, or both at the same time
     match opts.subcmd {
-        SubCommand::Test(t) => {
-            if t.debug {
-                println!("Printing debug info...");
-            } else {
-                println!("Printing normally...");
-            }
-        }
         SubCommand::UI(t) => {
-            startUI();
+            start_ui();
+        }
+        SubCommand::Box(t) => {
+            println!("{}", t.path);
+            start_box();
         }
     }
 }
@@ -86,11 +58,11 @@ fn is_atty(fileno: libc::c_int) -> bool {
     unsafe { libc::isatty(fileno) != 0 }
 }
 
-fn startUI() {
+fn start_box() {
     let stderr_is_atty = is_atty(libc::STDERR_FILENO);
 
     // initialise rustbox
-    let rustbox = match RustBox::init(InitOptions{
+    let rustbox = match RustBox::init(InitOptions {
         buffer_stderr: stderr_is_atty,
         input_mode: InputMode::Esc,
         output_mode: OutputMode::EightBit,
@@ -108,23 +80,24 @@ fn startUI() {
             Ok(rustbox::Event::KeyEvent(key)) => {
                 match key {
                     Key::Char('q') => { break; }
-                    _ => { }
+                    _ => {}
                 }
-            },
+            }
             Err(e) => panic!("{}", e.description()),
-            _ => { }
+            _ => {}
         }
     }
+}
 
-    //
-    // Application::new()
-    //     .window(|ctx| {
-    //         Window::new()
-    //             .title("OrbTk - minimal example")
-    //             .position((100.0, 100.0))
-    //             .size(420.0, 730.0)
-    //             .child(TextBlock::new().text("OrbTk")
-    //                 .build(ctx))
-    //             .build(ctx)
-    //     }).run();
+fn start_ui() {
+    Application::new()
+        .window(|ctx| {
+            Window::new()
+                .title("OrbTk - minimal example")
+                .position((100.0, 100.0))
+                .size(420.0, 730.0)
+                .child(TextBlock::new().text("OrbTk")
+                    .build(ctx))
+                .build(ctx)
+        }).run();
 }
