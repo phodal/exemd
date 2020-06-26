e86aa2796690f88ffb67f7c5252b49d336a39653
Phodal Huang <h@phodal.com>
2020-06-26 17:07:31 +0800

    feat: add simplify parser

--- src/rmd/parser.rs
+++ src/rmd/parser.rs
@@ -1,3 +1,8 @@
+use pulldown_cmark::{
+    Event::{Code, End, Html, Start, Text},
+    Options, Parser, Tag,
+};
+
 pub struct Rmd {
     text: String
 }
@@ -10,6 +15,36 @@ impl Rmd {
     }
 
     pub fn parse(&mut self) {
-        println!("start parse")
+        let parser = create_markdown_parser(&self.text);
+        let mut text = "".to_string();
+
+        for event in parser {
+            match event {
+                Start(tag) => {
+
+                }
+                End(tag) => {
+
+                }
+                Text(body) => {
+
+                }
+                Html(html) => {
+
+                }
+                Code(inline_code) => {
+                    text += &format!("`{}`", inline_code);
+                    println!("{}", text)
+                }
+                _ => (),
+            }
+        }
     }
+}
+
+fn create_markdown_parser(content: &String) -> Parser {
+    let mut options = Options::empty();
+    options.insert(Options::ENABLE_STRIKETHROUGH);
+    let parser = Parser::new_ext(&content, options);
+    parser
 }
